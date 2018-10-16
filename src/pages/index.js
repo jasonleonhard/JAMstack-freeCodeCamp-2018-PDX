b1dbf54d459614672af3cd6e094c1047f9d4f670
jasonleonhard <leonhard.jason@gmail.com>
2018-10-16 13:47:51 -0700

    first

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>JAMstack + freeCodeCamp 2018 PDX</h1>
+    <Link to="/page-2/">2</Link>
+  </Layout>
+)
+
+export default IndexPage
